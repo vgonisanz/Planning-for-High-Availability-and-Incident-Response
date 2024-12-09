d8c19208f90d0529f8e1ef62d5103bf6f77bcee6
Víctor Goñi <vgoni@veridas.com>
2024-12-09 10:35:23 +0100

    configure exercice 1 zone 1

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
@@ -1,16 +1,15 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-tf-tscotto6"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
- }
+  backend "s3" {
+    bucket = "udacity-tf-vgoni-4"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
 
- provider "aws" {
-   region = "us-east-2"
-   #profile = "default"
-   
-   default_tags {
-     tags = local.tags
-   }
- }
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = local.tags
+  }
+}
