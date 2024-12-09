de3d2232c11170124fe84db4ed4a3ef47fb43197
Víctor Goñi <vgoni@veridas.com>
2024-12-09 12:09:40 +0100

    fix: AMI from example

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -3,7 +3,7 @@ module "project_ec2" {
   instance_count     = 2
   name               = local.name
   account            = data.aws_caller_identity.current.account_id
-  aws_ami            = "ami-0b9064170e32bde34"
+  aws_ami            = "ami-0e0bf4b3a0c0e0adc"
   private_subnet_ids = module.vpc.private_subnet_ids
   public_subnet_ids  = module.vpc.public_subnet_ids
   vpc_id             = module.vpc.vpc_id
