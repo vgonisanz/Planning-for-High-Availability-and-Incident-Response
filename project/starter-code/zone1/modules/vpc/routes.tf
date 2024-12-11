9e21426475f4c15d18f827c5c589bbfb9e5ca750
Víctor Goñi <vgoni@veridas.com>
2024-12-11 13:35:26 +0100

    VCP configure to have only one

--- project/starter-code/zone1/modules/vpc/routes.tf
+++ project/starter-code/zone1/modules/vpc/routes.tf
@@ -27,7 +27,7 @@
  }
 
  resource "aws_route" "public_internet_gateway" {
-   count = length(var.azs) > 0 ? length(var.azs) : 0
+   count = 1
 
    route_table_id         = aws_route_table.public[0].id
    destination_cidr_block = "0.0.0.0/0"
