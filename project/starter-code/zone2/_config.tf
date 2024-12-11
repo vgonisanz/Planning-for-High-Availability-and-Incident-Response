d0c505bd0540d18710b61abd044cd17718ab6698
Víctor Goñi <vgoni@veridas.com>
2024-12-11 14:40:14 +0100

    Zone 2 update bucket name

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-vgoni-west"
+    bucket = "udacity-tf-vgoni-2-west"
     key    = "terraform/terraform.tfstate"
     region = "us-west-1"
   }
